532a48f843debc5f0491069b99f15ff5d50eed2a
Brendan Lee <lee33@uw.edu>
2015-10-29 15:01:17 -0700

    Added missing auth phase autosubmit

--- ServerOAuth/public/foursquare_final.js
+++ ServerOAuth/public/foursquare_final.js
@@ -90,6 +90,12 @@
       if (tableau.phase == tableau.phaseEnum.interactivePhase || tableau.phase == tableau.phaseEnum.authPhase) {
           if (hasAuth) {
               tableau.password = accessToken;
+              
+              if (tableau.phase == tableau.phaseEnum.authPhase) {
+                // Auto-submit here if we are in the auth phase
+                tableau.submit()
+              }
+              
               return;
           }
       }
